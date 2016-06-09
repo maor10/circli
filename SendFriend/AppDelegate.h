6b1932c99cca12ceb68f3afd735879b77660a7cf
Maor Kern <maor.kern@gmail.com>
2016-06-09 19:18:11 +0300

    Initial Commit

---
+++ SendFriend/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SendFriend
+//
+//  Created by Maor Kern on 6/9/16.
+//  Copyright © 2016 Maor Kern. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SendFriend/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SendFriend
+//
+//  Created by Maor Kern on 6/9/16.
+//  Copyright © 2016 Maor Kern. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
